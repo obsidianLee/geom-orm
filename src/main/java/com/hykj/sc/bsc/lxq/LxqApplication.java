15abc49d1afd49c562e02cfd8b514a8da3bc6fda
obsidianLee <obsidianx@qq.com>
2020-07-01 12:29:56 +0800

    update LxqApplication

--- src/main/java/com/hykj/sc/bsc/lxq/LxqApplication.java
+++ src/main/java/com/hykj/sc/bsc/lxq/LxqApplication.java
@@ -1,8 +1,5 @@
 package com.hykj.sc.bsc.lxq;
 
-import com.hykj.sc.bsc.lxq.repository.GridDORepository;
-import com.hykj.sc.bsc.lxq.service.GridService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,14 +14,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.hykj.sc.bsc.lxq"})
 public class LxqApplication implements ApplicationRunner {
 
-	@Autowired
-	GridService service;
-	@Autowired
-	GridDORepository gridRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LxqApplication.class, args);
-
 	}
 
 	@Override
